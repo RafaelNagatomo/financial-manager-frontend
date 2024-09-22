cba21e08aaa00d742393b968ea8d1a9be6b5f18c
Rafael Nagatomo <rafaelnagatomo@gmail.com>
2024-09-22 18:14:17 -0300

    config: react-query installed and config

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { CurrencyProvider } from './hooks/useCurrency';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from './styles/theme';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css'
 import './i18n'
@@ -12,23 +13,36 @@ import { CategoryProvider } from './contexts/CategoryContext'
 import { TransactionProvider } from './contexts/TransactionContext'
 import { GoalProvider } from './contexts/GoalContext'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <CurrencyProvider>
-        <ChakraProvider theme={theme}>
-          <CategoryProvider>
-            <TransactionProvider>
-              <GoalProvider>
-                <App />
-              </GoalProvider>
-            </TransactionProvider>
-          </CategoryProvider>
-        </ChakraProvider>
-      </CurrencyProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <CurrencyProvider>
+          <ChakraProvider theme={theme}>
+            <CategoryProvider>
+              <TransactionProvider>
+                <GoalProvider>
+                  <App />
+                </GoalProvider>
+              </TransactionProvider>
+            </CategoryProvider>
+          </ChakraProvider>
+        </CurrencyProvider>
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
